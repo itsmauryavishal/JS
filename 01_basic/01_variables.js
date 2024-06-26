5951a4861eaa20bf34b9625095c3ba4ca17fd1d4
itsmauryavishal <mauryavishal8983@gmail.com>
2024-06-26 02:10:14 +0000

    datatypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,24 @@
+ "use strict";  // treat all JS 
+ 
+ // alert(3+3)  // we are using nodejs, not browser
+
+ console.log(3+3)
+
+ console.log("Hitesh")
+
+
+ //   DATA TYPES
+
+//  Number
+//  BigInt
+//  String
+//  Boolean
+//  null
+//  undefined
+//  Symbol
+
+
+// object
+
+console.log(typeof undefined);   // undefined
+console.log(typeof null);    // object
