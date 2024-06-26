e3aa8f82e88eff69f48ea49dc8d8508c53118e40
itsmauryavishal <mauryavishal8983@gmail.com>
2024-06-26 10:58:27 +0000

    string

--- 01_basic/04_comparison.js
+++ 01_basic/04_comparison.js
@@ -17,3 +17,29 @@ console.log(null >= 0);
 
 
 console.log("2" === 2);
+
+
+const myFunction function(){
+    console.log("hello world");
+
+}
+
+//        *******  MEMORY     ******
+
+//      Stack(Primitive),    Heap (non-primitive)
+
+let myYoutubename ="vishal"
+
+let anothername= chaiaurcode
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne={
+    email:"user@google.com",
+    upi:"user@ybl"
+}
+
+
+
+
---
+++ 01_basic/05_strings.js
@@ -0,0 +1,25 @@
+const name ="hitesh"
+const repoCount=50
+
+// console.log(name + repoCount + " value")
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('vishal')
+
+console.log(gameName[0]);
+console.log(gameName._proto_);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase);
+console.log(gameName.charAt(2));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const newString = gameName.slice(-8,4)
+console.log(newString);
+
+
+
+
