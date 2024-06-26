f3c8b8adc10a7030353ed538ac2b9e70f11913e7
itsmauryavishal <mauryavishal8983@gmail.com>
2024-06-26 12:01:28 +0000

    dates

---
+++ 01_basic/07_dates.js
@@ -0,0 +1,22 @@
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(typeof myDate);
+
+// let myCreateDate = new Date(20123,0,23);
+// let myCreateDate = new Date(20123,0,23);
+
+
+//let myCreateDate = new Date(20123,0,23);
+
+//console.log(myCreateDate.toDateString());
+
+let myTimeStamp = Date.now()
+
+// console.log(mytimeStamp);
+// console.log(myCreateDate.getTime());
+
+//console.log(Date.now()/1000)
+let newDate= new Date();
+console.log(newDate);
+console.log(newDate.getMonth());
